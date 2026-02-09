83f9c83a4a3bfad8f900727afcef9cda394c2eff
bayezsyka <farrosy6@gmail.com>
2026-02-09 09:15:39 +0700

    npm

--- public/build/assets/ApplicationLogo-CQFnm9YQ.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-BSO6VoEY.js";function t(a){return o.jsx("img",{...a,src:"/images/logo.png",alt:"Logo Bendahara",className:`object-contain ${a.className}`})}export{t as A};
---
+++ public/build/assets/ApplicationLogo-DCX0GPCz.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-CiO2VQnx.js";function t(a){return o.jsx("img",{...a,src:"/images/logo.png",alt:"Logo Bendahara",className:`object-contain ${a.className}`})}export{t as A};
