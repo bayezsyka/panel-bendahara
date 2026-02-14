b3313ea3d0a73968950f2a4475d555ada5ec9ec7
bayezsyka <farrosy6@gmail.com>
2026-02-14 15:39:01 +0700

    npm

---
+++ public/build/assets/ApplicationLogo-afP2qbmV.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-D6V8vIYn.js";function t(a){return o.jsx("img",{...a,src:"/images/logo.png",alt:"Logo Bendahara",className:`object-contain ${a.className}`})}export{t as A};
--- public/build/assets/Select-BzXuO149.js
+++
@@ -1,8 +0,0 @@
-import{j as s}from"./app-DDkThiwp.js";function g({value:o,onChange:r,options:a=[],placeholder:l="-- Pilih --",className:i="",required:n=!1,disabled:t=!1,id:d,...u}){return s.jsxs("select",{id:d,value:o,onChange:r,required:n,disabled:t,className:`
-                block w-full rounded-lg border-gray-300 shadow-sm
-                text-sm text-gray-700
-                focus:border-indigo-500 focus:ring-indigo-500
-                disabled:bg-gray-100 disabled:cursor-not-allowed
-                transition-colors duration-150
-                ${i}
-            `,...u,children:[l&&s.jsx("option",{value:"",children:l}),a.map(e=>s.jsx("option",{value:e.value,children:e.label},e.value))]})}export{g as S};
